07b62b9f40f0c7a66772dba1e366763795c7599f
meh <meh@paranoici.org>
2012-08-18 22:06:57 +0200

    Improve themeability and add a theme wide indentation mechanism

--- lib/clit/theme.rb
+++ lib/clit/theme.rb
@@ -15,13 +15,25 @@ require 'terminfo'
 class Clit::Theme
 	include Singleton
 
-	attr_reader :name
+	attr_reader :name, :indentation
+
+	def initialize
+		@indentation = 0
+	end
 
 	def functions
-		methods - Object.instance_methods - [:functions, :name]
+		methods - Object.instance_methods - Clit::Theme.instance_methods
 	end
 
 	def new_line
 		print "\n"
+	end
+
+	def indent (size = 4)
+		@indentation += size
+
+		yield
+	ensure
+		@indentation -= size
 	end
 end
