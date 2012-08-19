4fd2ddd131cb2529c2d7ecd85598321e2497f081
CirKu17 <CirKu17@gmail.com>
2012-08-19 15:53:46 +0200

    added some features

--- lib/clit/themes/fancy.rb
+++ lib/clit/themes/fancy.rb
@@ -15,22 +15,84 @@ Clit.define_theme :fancy do
 	end
 
 	def warn (text)
-		puts "#{?⚠.yellow} #{text}"
+		puts "#{?✇.yellow} #{text}"
 	end
 
 	def error (text)
-		puts "#{?❗.red} #{text}"
+		puts "#{?⚠.red} #{text}"
 	end
 
 	def success (text)
 		puts "#{?✔.green} #{text}"
 	end
 
-	def failure (text)
+    def failure (text)
 		puts "#{?✘.red} #{text}"
 	end
 
 	def separator
 		puts ('─' * TermInfo.screen_size[1]).black.bold
 	end
+	
+	def waiting (text)
+        puts "#{?☕.white} #{text}"
+	end
+
+    def in_frame(text, width=0)
+
+      text.each_line { |line|
+        width = line.length if line.length >= width
+      }
+
+      print "┌"
+      (width+1).times { print "─" }
+      puts "┐"
+      text.each_line { |line|
+
+        print "│ " + line.tr("\n","")
+        (width-line.tr("\n","").length).times { print " " }
+        print "│"
+        puts
+      }
+      print "└"
+      (width+1).times { print "─" }
+      print "┘"
+      puts
+
+    end
+	
+    def draw_line_after(text, width=0)
+      
+      text.each_line { |line|
+        width = line.length if line.length >= width
+      }
+      
+      puts text
+      (width+1).times do
+      print "─"
+      end
+      puts
+      
+    end
+    
+    def with_bullets(array)
+      array.each do |i|
+        puts "• " + i
+      end
+    end
+    
+    def tree_list(array)
+      puts "│"
+      (array.length-1).times do |i|
+        puts "├── " + array[i]
+      end
+      puts "└── " + array.last
+    end
+    
+    def with_arrows(hash)
+      hash.each do |key, value|
+        puts "#{key} → #{value}"
+      end
+    end
+
 end
